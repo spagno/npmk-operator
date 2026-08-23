f9f6773f92d2fdb3c7bc6a27aae8801b7a5b7473
spagno <spagno@gmail.com>
2026-08-23 10:28:57 +0000

    fix: migrate API scheme registration

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,12 +1,19 @@
 package v1alpha1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 var (
 	GroupVersion  = schema.GroupVersion{Group: "npmko.io", Version: "v1alpha1"}
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
 	AddToScheme   = SchemeBuilder.AddToScheme
 )
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(GroupVersion, &Npmko{}, &NpmkoList{})
+	metav1.AddToGroupVersion(scheme, GroupVersion)
+	return nil
+}
